990a61d7798e2c7e29d6be9cb40ccca8929d28a7
PauloHS8 <paulohenrique62010@gmail.com>
2024-03-04 17:01:47 -0300

    Implementado a classe Aresta

--- src/main/java/br/edu/ufape/Aresta.java
+++ src/main/java/br/edu/ufape/Aresta.java
@@ -1,4 +1,39 @@
 package br.edu.ufape;
 
 public class Aresta {
+    private Vertice inicio;
+    private Vertice destino;
+    private double peso;
+
+    public Aresta(Vertice inicio, Vertice destino, double peso){
+        this.inicio = inicio;
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public Vertice getInicio() {
+        return this.inicio;
+    }
+
+    public void setInicio(Vertice inicio) {
+        this.inicio = inicio;
+    }
+
+    public Vertice getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Vertice destino) {
+        this.destino = destino;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
+
+
