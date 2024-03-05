32df12250a2099a7b26bfeb3aecf857b0497908b
Carlos Eduardo Bahia <cadu29bahia@gmail.com>
2024-03-05 12:40:08 -0300

    Implementação do algoritmo de Dijsktra

---
+++ src/main/java/br/edu/ufape/DijsktraAlgorithm.java
@@ -0,0 +1,72 @@
+package br.edu.ufape;
+
+import java.util.*;
+
+public class DijsktraAlgorithm {
+    private Grafo grafo;
+
+    public DijsktraAlgorithm(Grafo grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Vertice> menorCaminhoVertices(Vertice origem, Vertice destino) {
+        Map<Vertice, Double> distancia = new HashMap<>();
+        Map<Vertice, Vertice> predecessores = new HashMap<>();
+        PriorityQueue<Vertice> filaPrioridade = new PriorityQueue<>(Comparator.comparingDouble(distancia::get));
+
+        for (Vertice vertice : grafo.getVertices()) {
+            if (vertice.equals(origem))
+                distancia.put(vertice, 0.0);
+            else
+                distancia.put(vertice, Double.MAX_VALUE);
+            predecessores.put(vertice, null);
+            filaPrioridade.add(vertice);
+        }
+
+        while (!filaPrioridade.isEmpty()) {
+            Vertice verticeAtual = filaPrioridade.poll();
+            if (verticeAtual.equals(destino))
+                break;
+
+            for (Aresta aresta : grafo.getListaAdjacencia().get(verticeAtual)) {
+                Vertice vizinho = aresta.getInicio().equals(verticeAtual) ? aresta.getDestino() : aresta.getInicio();
+                double pesoTotal = distancia.get(verticeAtual) + aresta.getPeso();
+                if (pesoTotal < distancia.get(vizinho)) {
+                    distancia.put(vizinho, pesoTotal);
+                    predecessores.put(vizinho, verticeAtual);
+                    filaPrioridade.remove(vizinho); // Removendo e adicionando o vizinho para atualizar a fila de prioridade
+                    filaPrioridade.add(vizinho);
+                }
+            }
+        }
+
+        List<Vertice> caminho = new ArrayList<>();
+        Vertice verticeAtual = destino;
+        while (verticeAtual != null) {
+            caminho.add(verticeAtual);
+            verticeAtual = predecessores.get(verticeAtual);
+        }
+        Collections.reverse(caminho);
+
+        return caminho;
+    }
+
+
+    public List<Aresta> menorCaminhoArestas(List<Vertice> verticesCaminho) {
+        List<Aresta> arestasCaminho = new ArrayList<>();
+
+        for (int i = 0; i < verticesCaminho.size() - 1; i++) {
+            Vertice verticeAtual = verticesCaminho.get(i);
+            Vertice proximoVertice = verticesCaminho.get(i + 1);
+            for (Aresta aresta : grafo.getListaAdjacencia().get(verticeAtual)) {
+                if ((aresta.getInicio().equals(verticeAtual) && aresta.getDestino().equals(proximoVertice)) ||
+                        (aresta.getInicio().equals(proximoVertice) && aresta.getDestino().equals(verticeAtual))) {
+                    arestasCaminho.add(aresta);
+                    break;
+                }
+            }
+        }
+
+        return arestasCaminho;
+    }
+}
---
+++ src/test/java/br/edu/ufape/DijsktraAlgorithmTest.java
@@ -0,0 +1,113 @@
+import br.edu.ufape.*;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DijsktraAlgorithmTest {
+
+    @Test
+    public void testMenorCaminhoVertices() {
+        // Criação do grafo de teste
+        Grafo grafo = new Grafo();
+        Vertice v0 = new Vertice(0);
+        Vertice v1 = new Vertice(1);
+        Vertice v2 = new Vertice(2);
+        Vertice v3 = new Vertice(3);
+        Vertice v4 = new Vertice(4);
+        grafo.adicionarVertice(v0);
+        grafo.adicionarVertice(v1);
+        grafo.adicionarVertice(v2);
+        grafo.adicionarVertice(v3);
+        grafo.adicionarVertice(v4);
+
+        Aresta a0_1 = new Aresta("Rua 1", v0, v1, 10.0);
+        Aresta a0_2 = new Aresta("Rua 2", v0, v2, 5.0);
+        Aresta a1_2 = new Aresta("Rua 3", v1, v2, 2.0);
+        Aresta a1_3 = new Aresta("Rua 4", v1, v3, 30.0);
+        Aresta a2_1 = new Aresta("Rua 5", v2, v1, 3.0);
+        Aresta a2_3 = new Aresta("Rua 6", v2, v3, 9.0);
+        Aresta a2_4 = new Aresta("Rua 7", v2, v4, 2.0);
+        Aresta a3_4 = new Aresta("Rua 8", v3, v4, 4.0);
+        Aresta a4_0 = new Aresta("Rua 9", v4, v0, 8.0);
+
+        grafo.adicionarAresta(a0_1);
+        grafo.adicionarAresta(a0_2);
+        grafo.adicionarAresta(a1_2);
+        grafo.adicionarAresta(a1_3);
+        grafo.adicionarAresta(a2_1);
+        grafo.adicionarAresta(a2_3);
+        grafo.adicionarAresta(a2_4);
+        grafo.adicionarAresta(a3_4);
+        grafo.adicionarAresta(a4_0);
+
+        DijsktraAlgorithm dijska = new DijsktraAlgorithm(grafo);
+
+        // Teste do menor caminho entre v0 e v4
+        List<Vertice> menorCaminho = dijska.menorCaminhoVertices(v0, v4);
+        List<Vertice> expectedCaminho = Arrays.asList(v0, v2, v4);
+        assertEquals(expectedCaminho, menorCaminho);
+
+        // Teste do menor caminho entre v3 e v1
+        menorCaminho = dijska.menorCaminhoVertices(v3, v1);
+        expectedCaminho = Arrays.asList(v3, v4, v2, v1);
+        assertEquals(expectedCaminho, menorCaminho);
+    }
+
+    @Test
+    public void testMenorCaminhoArestas() {
+        // Criação do grafo de teste
+        Grafo grafo = new Grafo();
+        Vertice v0 = new Vertice(0);
+        Vertice v1 = new Vertice(1);
+        Vertice v2 = new Vertice(2);
+        Vertice v3 = new Vertice(3);
+        Vertice v4 = new Vertice(4);
+        grafo.adicionarVertice(v0);
+        grafo.adicionarVertice(v1);
+        grafo.adicionarVertice(v2);
+        grafo.adicionarVertice(v3);
+        grafo.adicionarVertice(v4);
+
+        Aresta a0_1 = new Aresta("Rua 1", v0, v1, 10.0);
+        Aresta a0_2 = new Aresta("Rua 2", v0, v2, 5.0);
+        Aresta a1_2 = new Aresta("Rua 3", v1, v2, 2.0);
+        Aresta a1_3 = new Aresta("Rua 4", v1, v3, 30.0);
+        Aresta a2_1 = new Aresta("Rua 5", v2, v1, 3.0);
+        Aresta a2_3 = new Aresta("Rua 6", v2, v3, 9.0);
+        Aresta a2_4 = new Aresta("Rua 7", v2, v4, 2.0);
+        Aresta a3_4 = new Aresta("Rua 8", v3, v4, 4.0);
+        Aresta a4_0 = new Aresta("Rua 9", v4, v0, 8.0);
+
+        grafo.adicionarAresta(a0_1);
+        grafo.adicionarAresta(a0_2);
+        grafo.adicionarAresta(a1_2);
+        grafo.adicionarAresta(a1_3);
+        grafo.adicionarAresta(a2_1);
+        grafo.adicionarAresta(a2_3);
+        grafo.adicionarAresta(a2_4);
+        grafo.adicionarAresta(a3_4);
+        grafo.adicionarAresta(a4_0);
+
+        DijsktraAlgorithm dijska = new DijsktraAlgorithm(grafo);
+
+        // Teste do menor caminho entre v0 e v4
+        List<Vertice> menorCaminhoVertices = dijska.menorCaminhoVertices(v0, v4);
+        List<Aresta> menorCaminhoArestas = dijska.menorCaminhoArestas(menorCaminhoVertices);
+
+        assertEquals(2, menorCaminhoArestas.size());
+        assertEquals(a0_2, menorCaminhoArestas.get(0));
+        assertEquals(a2_4, menorCaminhoArestas.get(1));
+
+        // Teste do menor caminho entre v3 e v1
+        menorCaminhoVertices = dijska.menorCaminhoVertices(v3, v1);
+        menorCaminhoArestas = dijska.menorCaminhoArestas(menorCaminhoVertices);
+
+        assertEquals(3, menorCaminhoArestas.size());
+        assertEquals(a3_4, menorCaminhoArestas.get(0));
+        assertEquals(a2_4, menorCaminhoArestas.get(1));
+        assertEquals(a1_2, menorCaminhoArestas.get(2));
+    }
+}
